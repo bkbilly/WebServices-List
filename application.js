f515da2ef10d23bec30bcef916331c201363ce9b
bkbilly <bkbilly@hotmail.com>
2015-12-07 02:01:34 +0200

    Added admin login, able to Update services data (only by Admin)

--- application.js
+++ application.js
@@ -1,81 +1,10 @@
 var hostname = window.location.hostname;
-var shapeshift;
+var bricksDiv;
+var shapeshiftOptions = {enableDrag: false}
+var shapeshiftObject;
+var loggedIn;
 var searchString = '';
-
-$( document ).ready(function() {
-	$('body').css('background-image','url(images/background.jpg)');
-	shapeshift = $(".shapeshift");
-	addServices(searchString);
-});
-
-$(document).on("ss-rearranged" ,function(e, selected) {
-	// console.log($(selected).index());
-	var options = {
-		enableDrag: false
-	}
-	var positions = []
-	shapeshift.children().each(function() {
-		var sv_id = $(this)[0].id.replace('service_', '');
-		var sv_order = $(this).index();
-		positions.push({
-			"sv_id": sv_id,
-			"sv_order": sv_order
-		});
-	});
-
-	$.post("dispatcher.php?action=updateOrder", {
-		positions
-	}).done(function( data ) {
-		console.log( data );
-	});
-});
-
-
-function searchServices(){
-	if (searchString !== $("#search").val()){
-		searchString = $("#search").val();
-		addServices(searchString);
-	}
-}
-
-function addServices(search){
-	shapeshift.empty();
-	$.getJSON( "dispatcher.php?action=getServices&search="+search, function(data) {
-		for (var i = 0; i < data.length; i++) {
-			var brick = getBrick(data[i]);
-			shapeshift.shapeshift();
-		};
-	}).fail(function(d, textStatus, error) {
-		var brick = '<div class="brick">\
-			<div class="cover">\
-				<h2>{name}</h2>\
-				<h4>{description}</h4>\
-			</div>\
-		</div>';
-		brick = brick.replace('{name}', textStatus);
-		brick = brick.replace('{description}', error);
-		shapeshift.append(brick);
-		shapeshift.shapeshift();
-		console.error("getJSON failed, status: " + textStatus + ", error: " + error);
-	});
-}
-
-function getBrick(service){
-	// console.log(service);
-	var serviceLength = Object.keys(service).length;
-	var sv_id = "service_" + service.sv_id;
-	var sv_status_id = "status_" + service.sv_id;
-	var name = service.sv_name;
-	var icon = "dispatcher.php?action=getImage&id=" + service.img_id;
-	var description = service.sv_description;
-	var target = service.sv_target;
-	var secured = service.sv_secured;
-	var port = service.sv_port;
-	var url = service.sv_url;
-	var http;
-	var porturl;
-
-	var brick = '<div id="{sv_id}" class="brick">\
+var brickSampleService = '<div id="{sv_id}" class="brick">\
 		<div class="brickBG" style="background-image: url(\'{icon}\')"></div>\
 		<a href="{porturl}">\
 			<div class="cover">\
@@ -85,31 +14,178 @@ function getBrick(service){
 			</div>\
 		</a>\
 	</div>';
-	brick_simple = '<div class="brick">\
+var brickSampleError = '<div class="brick">\
 		<div class="cover">\
 			<h2>{name}</h2>\
 			<h4>{description}</h4>\
 		</div>\
 	</div>';
+var brickSampleAdmin = '<div id="{sv_id}" class="brick">\
+		<div class="brickBG" style="background-image: url(\'{icon}\')"></div>\
+		<a href="javascript:adminPanel({service})">\
+			<div class="cover">\
+				<h2>{name}</h2>\
+				<h4>{description}</h4>\
+			</div>\
+		</a>\
+	</div>';
+
+
+$( document ).ready(function() {
+	$('body').css('background-image','url(images/background.jpg)');
+	bricksDiv = $(".shapeshift");
+	setLogin();
+});
+
+$(document).on("ss-rearranged" ,function(e, selected) {
+	// console.log($(selected).index());
+	var positions = []
+	bricksDiv.children().each(function() {
+		var sv_id = $(this)[0].id.replace('service_', '');
+		var sv_order = $(this).index();
+		positions.push({
+			"sv_id": sv_id,
+			"sv_order": sv_order
+		});
+	});
+	positions = {positions}
+
+	$.post("dispatcher.php?action=updateOrder", positions, function(data, textStatus) {
+		if(data['changed'] === false){
+			addServices(searchString);
+		}
+	}, "json");
+
+});
+
+function login(){
+	username = $('#inputUser').val();
+	password = $('#inputPass').val();
+	$.getJSON("dispatcher.php?action=login&user="+ username +"&password="+ password +"", function(response){
+		if(response['credentials'] === true){
+			$('#loginError').hide();
+			setLogin();
+		} else {
+			$('#loginError').fadeIn();
+		}
+	});
+}
+
+function logout(){
+	$.getJSON("dispatcher.php?action=logout", function(response){
+		setLogin();
+	});
+}
+
+function searchServices(){
+	if (searchString !== $("#search").val()){
+		searchString = $("#search").val();
+		addServices(searchString);
+	}
+}
+
+function setLogin(){
+	$.getJSON("dispatcher.php?action=usrStatus", function(response){
+		if(response['connected'] === true){
+			$('#loginBtn').css('display','none');
+			$('#logoutBtn').css('display','block');
+			loggedIn = true;
+		}
+		else{
+			$('#loginBtn').css('display','block');
+			$('#logoutBtn').css('display','none');
+			loggedIn = false;
+		}
+		addServices(searchString);
+	});
+}
+
+function addServices(search){
+	bricksDiv.empty();
+	$.getJSON( "dispatcher.php?action=getServices&search="+search, function(data) {
+		for (var i = 0; i < data.length; i++) {
+			var brick = getBrick(data[i]);
+		};
+		if(loggedIn === true){
+			shapeshiftOptions.enableDrag = true;
+		} else {
+			shapeshiftOptions.enableDrag = false;
+		}
+		bricksDiv.shapeshift(shapeshiftOptions);
+	});
+}
+
+function adminPanel(sv_id){
+	$.getJSON( "dispatcher.php?action=getService&sv_id="+sv_id, function(data) {
+		$('#editName').val(data.sv_name);
+		$('#editDescription').val(data.sv_description);
+		$('#editTarget').val(data.sv_target);
+		$('#editURL').val(data.sv_url);
+		$('#editSecured').val(data.sv_secured);
+		$('#editIMG').val(data.img_id);
+
+		$('#myModal').modal();
+	});
+
+	$("#saveServiceBTN").off( "click" );
+	$("#saveServiceBTN").click(function() {
+		updateServiceData = {
+			'sv_id': sv_id,
+			'sv_name': $('#editName').val(),
+			'sv_description': $('#editDescription').val(),
+			'sv_target': $('#editTarget').val(),
+			'sv_url': $('#editURL').val(),
+			'sv_secured': $('#editSecured').val(),
+			'img_id': $('#editIMG').val()
+		}
+		$.post("dispatcher.php?action=updateService", updateServiceData, function(data, textStatus) {
+			if(data['changed'] === true){
+				addServices(searchString);
+			}
+		}, "json");
+	});
+}
+
+function getBrick(service){
+	var serviceLength = Object.keys(service).length;
+	var sv_id = service.sv_id;
+	var name = service.sv_name;
+	var img_id = service.img_id;
+	var description = service.sv_description;
+	var target = service.sv_target;
+	var secured = service.sv_secured;
+	var port = service.sv_port;
+	var url = service.sv_url;
+	var sv_service_id = "service_" + sv_id;
+	var sv_status_id = "status_" + sv_id;
+	var icon = "dispatcher.php?action=getImage&id=" + img_id;
+	var http;
+	var porturl;
+
 	if (serviceLength === 0) {
-		brick = brick_simple
+		var brick = brickSampleError
 		brick = brick.replace('{name}', "Error");
 		brick = brick.replace('{description}', "Not Found");
-	} else {
+	} else if(loggedIn === false){
+		var brick = brickSampleService;
 		if(secured === "true"){http = "https://";}else{http = "http://";}
 		if(target == "127.0.0.1"){hostname = window.location.hostname}else{hostname = target}
 		porturl = http + hostname + ":" + port + url;
 		brick = brick.replace('{porturl}', porturl);
 		brick = brick.replace('{icon}', icon);
+		CheckURL(sv_status_id, porturl);
+	} else if(loggedIn === true){
+		var brick = brickSampleAdmin;
+		brick = brick.replace('{service}', sv_id);
+		brick = brick.replace('{icon}', icon);
 	}
 
-	brick = brick.replace('{sv_id}', sv_id);
+	brick = brick.replace('{sv_id}', sv_service_id);
 	brick = brick.replace('{sv_status_id}', sv_status_id);
 	brick = brick.replace('{name}', name);
 	brick = brick.replace('{description}', description);
 	
-	shapeshift.append(brick);
-	CheckURL(sv_status_id, porturl);
+	bricksDiv.append(brick);
 
 	return brick;
 }
@@ -117,16 +193,11 @@ function getBrick(service){
 function CheckURL(sv_status_id, porturl){
 	green = "images/buttonGreen.png";
 	red = "images/buttonRed.png";
-	var xhttp = new XMLHttpRequest();
-	xhttp.open('GET', 'dispatcher.php?action=urlExists&url=' + porturl, true);
-	xhttp.onreadystatechange = function(){
-		if (xhttp.readyState == 4 && xhttp.status == 200) {  
-			response = xhttp.responseText;
-			if (response === 'true')
-				$('#'+sv_status_id).attr("src", green);
-			else
-				$('#'+sv_status_id).attr("src", red);
-		}
-	};
-	xhttp.send();
+	$.get("dispatcher.php?action=urlExists&url=" + porturl, function(response){
+		if (response === 'true')
+			$('#'+sv_status_id).attr("src", green);
+		else
+			$('#'+sv_status_id).attr("src", red);
+	});
+
 }
--- dispatcher.php
+++ dispatcher.php
@@ -13,8 +13,10 @@
 	$action = $_REQUEST['action'];
 	switch($action){
 		case 'getImage' : getImage($db); break;
+		case 'getService' : getService($db); break;
 		case 'getServices' : getServices($db); break;
 		case 'updateOrder' : updateOrder($db); break;
+		case 'updateService' : updateService($db); break;
 		case 'urlExists' : urlExists(); break;
 		case 'login' : login($db); break;
 		case 'logout' : logout(); break;
@@ -31,28 +33,31 @@
 		$row = $ret->fetchArray(SQLITE3_ASSOC);
 		if($row){
 			$_SESSION['UserName']=$user;
-			echo "successfully connected";
+			$status = array('credentials' => true);
 		}
 		else{
-			echo "failure connecting";
+			$status = array('credentials' => false);
 		}
+		echo json_encode($status);
 	}
 
 	function logout(){
 		session_start();
 		unset($_SESSION['UserName']);
 		session_destroy();
-		echo "successfully loged out";
+		$status = array('logout' => true);
+		echo json_encode($status);
 	}
 
 	function usrStatus(){
 		session_start();
 		if(isset($_SESSION['UserName'])){
-			echo "connected";
+			$status = array('connected' => true);
 		}
 		else{
-			echo "not connected";
+			$status = array('connected' => false);
 		}
+		echo json_encode($status);
 	}
 
 	function getImage($db){
@@ -65,6 +70,17 @@
 		echo $row['img_upload'];
 	}
 
+	function getService($db){
+		$svId = $_REQUEST['sv_id'];
+		$sql = "SELECT * FROM services WHERE sv_id=$svId";
+		$ret = $db->query($sql);
+		$service = array();
+		while($row = $ret->fetchArray(SQLITE3_ASSOC) ){
+			$service = $row;
+		}
+		echo json_encode($service);
+	}
+
 	function getServices($db){
 		$search = $_REQUEST['search'];
 		$sql = "SELECT * FROM services WHERE sv_name like '%$search%' or sv_description like '%$search%' ORDER BY sv_order ASC";
@@ -73,22 +89,64 @@
 		while($row = $ret->fetchArray(SQLITE3_ASSOC) ){
 			$services[] = $row;
 		}
-		echo(json_encode($services));
+		echo json_encode($services);
 	}
 
 	function updateOrder($db){
-		$new_order = $_POST;
-		foreach ($new_order['positions'] as $value) {
-			$sv_id = $value['sv_id'];
-			$sv_order = $value['sv_order'];
-			$sql = "UPDATE services SET sv_order=$sv_order WHERE sv_id=$sv_id";
-			$ret = $db->exec($sql);
-			if(!$ret){
-				echo $db->lastErrorMsg();
-			} else {
-				echo $db->changes(), " Record updated successfully";
+		session_start();
+		if(isset($_SESSION['UserName'])){
+			$new_order = $_POST;
+			foreach ($new_order['positions'] as $value) {
+				$sv_id = $value['sv_id'];
+				$sv_order = $value['sv_order'];
+				$sql = "UPDATE services SET sv_order=$sv_order WHERE sv_id=$sv_id";
+				$ret = $db->exec($sql);
+				if(!$ret){
+					$status = array('changed' => false, 'message' => $db->lastErrorMsg());
+					break;
+				} else {
+					if($db->changes() == 1){
+						$status = array('changed' => true, 'message' => "Successfully changed order");
+					} else{
+						$status = array('changed' => false, 'message' => "Can't change order");
+					}
+				}
 			}
 		}
+		else{
+			$status = array('changed' => false, 'message' => "User not connected");
+		}
+		echo json_encode($status);
+	}
+
+	function updateService($db){
+		session_start();
+		if(isset($_SESSION['UserName'])){
+			$value = $_POST;
+			$sv_id = $value['sv_id'];
+			$sv_name = $value['sv_name'];
+			$sv_description = $value['sv_description'];
+			$sv_target = $value['sv_target'];
+			$sv_url = $value['sv_url'];
+			$sv_secured = $value['sv_secured'];
+			$img_id = $value['img_id'];
+			$sql = "UPDATE services SET sv_name='$sv_name', sv_description='$sv_description', sv_target='$sv_target', sv_url='$sv_url', sv_secured='$sv_secured', img_id=$img_id WHERE sv_id=$sv_id";
+			$ret = $db->exec($sql);
+			if(!$ret){
+				$status = array('changed' => false, 'message' => $db->lastErrorMsg());
+				break;
+			} else {
+				if($db->changes() == 1){
+					$status = array('changed' => true, 'message' => "Successfully updated service");
+				} else{
+					$status = array('changed' => false, 'message' => "Can't update service");
+				}
+			}
+		}
+		else{
+			$status = array('changed' => false, 'message' => "User not connected");
+		}
+		echo json_encode($status);
 	}
 
 	function urlExists(){
